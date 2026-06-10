10a6aaeca4b81f6ec90168a38c8b3cfad6940dd9
valmikiarunkumar15 <Valmiki.ArunKumar@cognizant.com>
2026-06-10 09:03:09 +0530

    Working on Production Service and Controller

--- src/main/java/com/genc/smpps/service/ProductionOrderService.java
+++ src/main/java/com/genc/smpps/service/ProductionOrderService.java
@@ -1,5 +1,24 @@
 package com.genc.smpps.service;
 
 
+import com.genc.smpps.model.ProductionOrder;
+import com.genc.smpps.repo.ProductionOrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductionOrderService {
+
+    @Autowired
+    private ProductionOrderRepository repo;
+
+    // Create Order
+    public ProductionOrder createProductionOrder(ProductionOrder order) {
+        order.setOrderStatus("PLANNED");
+        order.setProducedQuantity(0); // ensure default
+        return repo.save(order);
+    }
+
 }
