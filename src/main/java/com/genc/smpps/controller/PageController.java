309a6d2bd3fee0c7d61dec0044e1ad869d5dceba
Gowtham <2503807@cognizant.com>
2026-06-14 22:56:20 +0530

    Updated page controller

--- src/main/java/com/genc/smpps/controller/PageController.java
+++ src/main/java/com/genc/smpps/controller/PageController.java
@@ -1,9 +1,37 @@
 package com.genc.smpps.controller;
 
 
+import com.genc.smpps.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class PageController {
+    @Autowired
+    private ProductMasterService productService;
+    @Autowired
+    private ProductionOrderService productionOrderService;
+    @Autowired
+    private MachineService machineService;
+    @Autowired
+    private QualityService qualityService;
+    @Autowired
+    private MaintenanceService maintenanceService;
+
+    @GetMapping("/")
+    public String dashboard(Model model) {
+
+        model.addAttribute("maintenanceCount", maintenanceService.getAllWorkOrders().size());
+
+        return "index";
+    }
 
 
-
+    @GetMapping("/maintenance-page")
+    public String maintenancePage(Model model) {
+        model.addAttribute("orders", maintenanceService.getAllWorkOrders());
+        return "maintenance";
+    }
 }
