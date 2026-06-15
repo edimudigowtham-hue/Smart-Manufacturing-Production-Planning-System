b0a0f901f535f84ab4301bf52898673e09c0524d
sohankumarmiryala-office <sohankumar.miryala@cognizant.com>
2026-06-15 10:09:09 +0530

    Update MachineService.java

--- src/main/java/com/genc/smpps/service/MachineService.java
+++ src/main/java/com/genc/smpps/service/MachineService.java
@@ -1,5 +1,29 @@
 package com.genc.smpps.service;
 
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.genc.smpps.model.MachineLog;
+import com.genc.smpps.repo.MachineLogRepository;
+
+@Service
 public class MachineService {
+
+    @Autowired
+    private MachineLogRepository repo;
+
+    // recordRuntime()
+    public MachineLog recordRuntime(MachineLog log) {
+        log.setMachineStatus("RUNNING");
+        return repo.save(log);
+    }
+
+    // logDowntime()
+    public MachineLog logDowntime(MachineLog log) {
+        log.setMachineStatus("BREAKDOWN");
+        return repo.save(log);
+    }
+    
 }
