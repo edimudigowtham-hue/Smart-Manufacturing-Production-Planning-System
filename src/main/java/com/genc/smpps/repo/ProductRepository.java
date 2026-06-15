5bb808c7fdfbedc6837f3b501b2e0ed553901cd8
Kummithi <2503815@cognizant.com>
2026-06-15 08:59:58 +0530

    Added Repository file

--- src/main/java/com/genc/smpps/repo/ProductRepository.java
+++ src/main/java/com/genc/smpps/repo/ProductRepository.java
@@ -1,7 +1,7 @@
 package com.genc.smpps.repo;
 
-import com.genc.smpps.model.FinishedProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.genc.smpps.model.FinishedProduct;
 
-public interface ProductRepository{
+public interface ProductRepository extends JpaRepository<FinishedProduct, Integer> {
 }
