d555aa0c4c380a058bb7142b058b06c1efd45d6b
Kummithi <2503815@cognizant.com>
2026-06-15 09:03:08 +0530

    Updated ProductMasterService class

--- src/main/java/com/genc/smpps/service/ProductMasterService.java
+++ src/main/java/com/genc/smpps/service/ProductMasterService.java
@@ -1,6 +1,31 @@
 package com.genc.smpps.service;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.genc.smpps.model.FinishedProduct;
+import com.genc.smpps.repo.ProductRepository;
 
+@Service
 public class ProductMasterService {
 
+    @Autowired
+    private ProductRepository repo;
+
+    public FinishedProduct createProduct(FinishedProduct product) {
+        return repo.save(product);
     }
+
+    public FinishedProduct updateBomVersion(int id, String version) {
+        FinishedProduct p = repo.findById(id).orElse(null);
+        if (p != null) {
+            p.setBomVersion(version);
+            return repo.save(p);
+        }
+        return null;
+    }
+
+    public List<FinishedProduct> getAllProducts() {
+        return repo.findAll();
+    }
+}
